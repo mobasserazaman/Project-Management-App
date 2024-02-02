12a91f04b1170cd3e21650b2b60c0e4ca2242309
Mobassera Zaman <mobassera.zaman@mail.mcgill.ca>
2024-02-02 14:25:36 -0500

    Validate Input and show Error Modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,22 @@
 import { useRef } from "react"
 import Input from "./Input"
-
+import Modal from "./Modal";
 export default function NewProject({onAdd}) {
 
     const title = useRef();
     const description = useRef();
     const date = useRef();
+    const modal = useRef();
 
     function handleSave(){
         const enteredTitle = title.current.value;
         const enteredDescription = description.current.value;
         const enteredDate = date.current.value;
+
+        if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDate.trim() === ''){
+            modal.current.open(); 
+            return;
+        } 
 
         onAdd({
             title : enteredTitle,
@@ -20,7 +26,17 @@ export default function NewProject({onAdd}) {
     }
 
 
-    return <div className="w-[35rem] mt-16">
+    return(
+      <>
+    <Modal ref={modal} buttonCaption="Close">
+        
+        <h2>Invalid Input</h2>
+        <p>Looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input.</p>
+        
+        
+    </Modal>
+    <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-3 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
             <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
@@ -31,4 +47,8 @@ export default function NewProject({onAdd}) {
             <Input type='date' ref={date} label="Date"/>
         </div>
     </div>
+    </>  
+    )
+    
+     
 }
